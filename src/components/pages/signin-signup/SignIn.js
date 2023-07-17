edbf9c67866e00fa73581515961462e311b19fef
AshishDhimal <yonyangkamal11@gmail.com>
2023-07-17 23:54:03 +1000

    signin

--- src/components/pages/signin-signup/SignIn.js
+++ src/components/pages/signin-signup/SignIn.js
@@ -1,12 +1,48 @@
 import React from "react";
 import Header from "../../layout/Header";
 import Footer from "../../layout/Footer";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import CustomInput from "../../custom-input/CustomInput";
 
 const SignIn = () => {
+  const inputs = [
+    {
+      label: "Email",
+      name: "email",
+      required: true,
+      placeholder: "Sam@smit.com",
+      type: "email",
+    },
+    {
+      label: "Password",
+      name: "password",
+      required: true,
+      placeholder: "******",
+      type: "password",
+      minLength: "6",
+    },
+  ];
+
   return (
     <div>
       <Header />
-      <div className="main"></div>
+      <section className="main">
+        <Form className="m-5 p-5 border shadow-lg">
+          <h1>Welcome Admins Login Now</h1>
+          <hr />
+
+          {inputs.map((item, i) => (
+            <CustomInput key={i} {...item} />
+          ))}
+
+          <div className="d-grid">
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </div>
+        </Form>
+      </section>
       <Footer />
     </div>
   );
