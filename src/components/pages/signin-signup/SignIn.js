4f625a7907b8269e7ac147a37f84c56c426f7dec
AshishDhimal <yonyangkamal11@gmail.com>
2023-08-02 23:18:40 +1000

    u

--- src/components/pages/signin-signup/SignIn.js
+++ src/components/pages/signin-signup/SignIn.js
@@ -8,16 +8,19 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { SignInAdminAction, autoLogin } from "../adminAction";
 import { useDispatch, useSelector } from "react-redux";
 
+const initialState = {
+  email: "",
+  password: "",
+};
 const SignIn = () => {
   const location = useLocation();
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [form, setForm] = useState({});
-
+  const [form, setForm] = useState(initialState);
   const { admin } = useSelector((state) => state.adminInfo);
 
   const pathTo = location.state?.from?.location?.pathname || "/dashboard";
-
   useEffect(() => {
     admin?._id && navigate(pathTo);
     dispatch(autoLogin());
@@ -37,45 +40,47 @@ const SignIn = () => {
     dispatch(SignInAdminAction(form));
   };
 
-  const inputs = [
-    {
-      label: "Email",
-      name: "email",
-      required: true,
-      placeholder: "Sam@smit.com",
-      type: "email",
-    },
-    {
-      label: "Password",
-      name: "password",
-      required: true,
-      placeholder: "******",
-      type: "password",
-    },
-  ];
-
   return (
     <div>
       <Header />
-      <section className="main">
-        <Form onSubmit={handleOnSubmit} className="m-5 p-5 border shadow-lg">
-          <h1>Welcome Back</h1>
-          <hr />
+      <main className="main pt-5">
+        <Form
+          onSubmit={handleOnSubmit}
+          className="border p-3 shadow-lg"
+          style={{ width: "450px", margin: "auto" }}
+        >
+          <h1 className="mb-5">
+            Welcome Back
+            <hr />
+          </h1>
 
-          {inputs.map((item, i) => (
-            <CustomInput key={i} {...item} onChange={handleOnChange} />
-          ))}
+          <CustomInput
+            label="Email"
+            name="email"
+            type="email"
+            required
+            value={form.email}
+            onChange={handleOnChange}
+          />
+          <CustomInput
+            label="Password"
+            name="password"
+            type="password"
+            value={form.password}
+            onChange={handleOnChange}
+            required
+          />
 
-          <div className="d-grid">
+          <div className="d-grid mt-3">
             <Button variant="dark" type="submit">
               Sign In
             </Button>
           </div>
           <p className="mt-2 text-end">
-            Forgot Password? <Link to="ottp">Reset</Link> now
+            Forget password? <Link to="reset-pass-otp">reset </Link> now.
           </p>
         </Form>
-      </section>
+      </main>
       <Footer />
     </div>
   );
