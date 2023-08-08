0815a3038a2735cf5e4db9e147384d5245424a08
AshishDhimal <yonyangkamal11@gmail.com>
2023-08-08 22:07:09 +1000

    u

--- src/components/paymentType/PaymentAction.js
+++ src/components/paymentType/PaymentAction.js
@@ -1,6 +1,11 @@
 import { toast } from "react-toastify";
 import { getPayment, postPayment } from "../../helper/axios";
 import { setPayments } from "../pages/payment-option/PaymentSlice";
+import {
+  deletePaymentById,
+  updatePaymentById,
+} from "../../../src/model/payment/PaymentModel";
+import { setModelShow } from "../../system/systemSlice";
 
 export const postNewPaymentAction = (obj) => async (dispatch) => {
   const { status, message } = await postPayment(obj);
@@ -18,4 +23,33 @@ export const getPaymentAction = () => async (dispatch) => {
   if (status === "success") {
     dispatch(setPayments(result));
   }
+};
+
+export const updatePaymentAction = (data) => async (dispatch) => {
+  const pending = updatePaymentById(data);
+
+  toast.promise(pending, {
+    pending: "Please wait...",
+  });
+
+  const { status, message } = await pending;
+
+  toast[status](message);
+
+  status === "success" && dispatch(getPaymentAction());
+};
+
+export const deletePaymentAction = (_id) => async (dispatch) => {
+  const pending = deletePaymentById(_id);
+
+  toast.promise(pending, {
+    pending: "Please wait...",
+  });
+
+  const { status, message } = await pending;
+
+  toast[status](message);
+
+  status === "success" && dispatch(getPaymentAction());
+  dispatch(setModelShow(false));
 };
