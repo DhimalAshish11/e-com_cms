2f975aecfa35376d347e8c66a62cb6275b4ab4dc
AshishDhimal <yonyangkamal11@gmail.com>
2023-08-28 09:45:52 +1000

    u

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,8 +1,56 @@
 import React from "react";
 import AdminLayout from "../../layout/AdminLayout";
+import { Button } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import { useSelector } from "react-redux";
 
 export const Dashboard = () => {
-  return <AdminLayout title="Dashboard">Dashboard</AdminLayout>;
+  const { admin } = useSelector((state) => state.adminInfo);
+
+  return (
+    <AdminLayout title="Dashboard">
+      <div className="card d-flex gap-4 p-3 m-5">
+        <Card className="bg-success text-light">
+          <Card.Body>
+            <Card.Title>{admin.fName}</Card.Title>
+            <Card.Text>Welcome to DashBoard </Card.Text>
+            <Card.Footer>
+              <Button>More Info</Button>
+            </Card.Footer>
+          </Card.Body>
+        </Card>
+
+        <Card className="bg-primary text-light">
+          <Card.Body>
+            <Card.Title> uuuuu </Card.Title>
+            <Card.Text>Books Found </Card.Text>
+            <Card.Footer>
+              <Button>More Info</Button>
+            </Card.Footer>
+          </Card.Body>
+        </Card>
+
+        <Card className="bg-danger text-light">
+          <Card.Body>
+            <Card.Title>hhhh</Card.Title>
+            <Card.Text>Borrowed Books Found </Card.Text>
+            <Card.Footer>
+              <Button>More Info</Button>
+            </Card.Footer>
+          </Card.Body>
+        </Card>
+        <Card className="bg-warning text-light">
+          <Card.Body>
+            <Card.Title> tyyui </Card.Title>
+            <Card.Text>Reviews Found </Card.Text>
+            <Card.Footer>
+              <Button>More Info</Button>
+            </Card.Footer>
+          </Card.Body>
+        </Card>
+      </div>
+    </AdminLayout>
+  );
 };
 
 export default Dashboard;
